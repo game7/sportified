359f458cf424163df322115726a28a71239e58a6
game7 <cmwoodall@game7.net>
2011-01-10 16:15:18 -0700

    remove unnecessary scope entry

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Sportified::Application.routes.draw do
 
-
   get "home/index"
 
   root :to => "home#index"
@@ -10,12 +9,10 @@ Sportified::Application.routes.draw do
   match "/league" => "league/home#index", :as => :league
   namespace :league do
     resources :divisions, :shallow => true do
-      scope "/league" do
-        resources :seasons, :shallow => true do
-          resources :games
-          resources :teams, :shallow => true do
-            resources :players
-          end
+      resources :seasons, :shallow => true do
+        resources :games
+        resources :teams, :shallow => true do
+          resources :players
         end
       end
     end
