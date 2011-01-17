47a9c4b221476fcc0007bee1733b43dc303e29a7
game7 <cmwoodall@game7.net>
2011-01-17 09:39:43 -0700

    seed some league data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,19 @@ Mongoid.master.collections.reject { |c| c.name == 'system.indexes' }.each(&:drop
 puts 'SETTING UP DEFAULT USER LOGIN'
 user = User.create! :name => 'First User', :email => 'first_user@test.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user.name
+
+puts 'SETTING UP DEMO LEAGUE'
+division = Division.create! :name => 'Advanced'
+season = division.seasons.build :name => 'Winter 2011', :starts_on => '1/1/2011', :ends_on => '4/1/2011'
+season.save
+kings = season.teams.build :name => 'Kings'
+kings.save
+ducks = season.teams.build :name => 'Ducks'
+ducks.save
+sharks = season.teams.build :name => 'Sharks'
+sharks.save
+stars = season.teams.build :name => 'Stars'
+stars.save
+
+
+
