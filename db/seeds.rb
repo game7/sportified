132d4979c0a8139e42ac8ec0fbfde2867370c957
game7 <cmwoodall@game7.net>
2011-01-21 15:28:11 -0700

    uppdated seeds to add games

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,49 @@ puts 'SETTING UP DEMO LEAGUE'
 division = Division.create! :name => 'Advanced'
 season = division.seasons.build :name => 'Winter 2011', :starts_on => '1/1/2011', :ends_on => '4/1/2011'
 season.save
-kings = season.teams.build :name => 'Kings'
-kings.save
-ducks = season.teams.build :name => 'Ducks'
-ducks.save
-sharks = season.teams.build :name => 'Sharks'
-sharks.save
-stars = season.teams.build :name => 'Stars'
-stars.save
+
+kings = season.teams.create :name => 'Kings'
+ducks = season.teams.create :name => 'Ducks'
+sharks = season.teams.create :name => 'Sharks'
+stars = season.teams.create :name => 'Stars'
+
+puts 'GENERATING GAMES'
+game_times = [ DateTime.parse('1/13/2011 9:00 pm'), DateTime.parse('1/13/2011 10:30 pm') ]
+teams = season.teams.all.entries
+team_count = teams.count
+rounds = 10
+games_per_round = team_count / 2
+
+puts '- ' << team_count.to_s << ' teams'
+puts '- ' << rounds.to_s << ' rounds'
+
+rounds.times do |round|
+  puts '-- Round ' << round.to_s << ' --'
+  games_per_round.times do |game|
+    if round % 2 == 1
+      @left_team = teams[0 + game]
+      @right_team = teams[team_count - game - 1]
+    else
+      @right_team = teams[0 + game]
+      @left_team = teams[team_count - game - 1]      
+    end
+    puts ' ' << @left_team.name << ' vs ' << @right_team.name
+    @days_advance = round * 7
+    g = season.games.build
+    g.starts_on = game_times[game].advance(:days => @days_advance)
+    g.left_team = GameTeam.new( :team => @left_team )
+    g.right_team = GameTeam.new( :team => @right_team )
+    g.save
+  end
+  #shift teams
+  @a = teams.shift
+  @b = teams.shift
+  teams.push @b
+  teams.unshift @a
+  #shit times
+  @a = game_times.shift
+  game_times.push @a
+end
 
 
 
