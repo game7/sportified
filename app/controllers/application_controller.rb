bed00589f952fe2e38d1fb1c0e971f1b82af5c5a
game7 <cmwoodall@game7.net>
2011-01-24 23:35:26 -0700

    added auto-loading of controller/action-specific stylesheets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper :layout
+
+  before_filter :add_stylesheets
+
+  def initialize
+    super
+    @stylesheets = []
+  end
+
+  def add_stylesheets
+    # http://push.cx/2006/tidy-stylesheets-in-rails
+    ["#{controller_path}/shared", "#{controller_path}/#{action_name}"].each do |stylesheet|
+      @stylesheets << stylesheet if File.exists? "#{RAILS_ROOT}/public/stylesheets/#{stylesheet}.css"
+    end
+  end
+
 end
