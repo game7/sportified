b3ff7ab0d44c190c31b40eafe25d411610ee1de4
game7 <cmwoodall@game7.net>
2011-02-03 11:50:13 -0700

    fixed javascript issue with clearForm

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,9 +6,9 @@ function clearForm(form) {
   $(':input', form).each(function() {
     var type = this.type;
     var tag = this.tagName.toLowerCase();
-    if (type == 'text' || type = 'password' || tag == 'textarea')
+    if (type == 'text' || type == 'password' || tag == 'textarea')
       this.value = "";
-    else if (type = 'checkbox' || type == 'radio')
+    else if (type == 'checkbox' || type == 'radio')
       this.checked = false;
     else if (tag == 'select')
       this.selectedIndex = -1;
