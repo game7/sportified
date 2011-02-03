f6cc648a72fcf5ad8192b159686a150e8015609b
game7 <cmwoodall@game7.net>
2011-02-03 11:36:39 -0700

    corrected issue that prevented subsequent remote form posting

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+function clearForm(form) {
+  $(':input', form).each(function() {
+    var type = this.type;
+    var tag = this.tagName.toLowerCase();
+    if (type == 'text' || type = 'password' || tag == 'textarea')
+      this.value = "";
+    else if (type = 'checkbox' || type == 'radio')
+      this.checked = false;
+    else if (tag == 'select')
+      this.selectedIndex = -1;
+  });
+};
