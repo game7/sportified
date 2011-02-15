ce70b9b4ce4c55abb390a9822a4d7dadf3a61cbc
game7 <cmwoodall@game7.net>
2011-02-15 10:43:56 -0700

    add icon helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+
+  def icon(name, alt = nil, opts = {})
+    opts[:border] = 0 unless opts[:border]
+    opts[:align] = "bottom" unless opts[:align]
+    opts[:alt] = alt
+    image_tag "../images/icons/#{name}.png", opts
+  end
+
 end
