07b4b8ea35b625c20f40b37b67e569f766da827b
game7 <cmwoodall@game7.net>
2011-03-02 16:56:53 -0700

    trying mongoid path for many/many - not sure if working

---
+++ lib/patches/mongoid.rb
@@ -0,0 +1,33 @@
+module Mongoid #:nodoc:
+  # do NOT validate the other side of references_and_referenced_in_many association
+  module Relations #:nodoc:
+    
+    class Many < Proxy
+      def <<(*args)
+        options = default_options(args)
+        args.flatten.each do |doc|
+          return doc unless doc
+          append(doc, options)
+          doc.save(:validate => false) if base.persisted? && !options[:binding]
+        end
+      end
+    end
+    
+    module Referenced #:nodoc:
+      class Many < Relations::Many
+        def bind(options = {})
+          binding.bind(options)
+          target.map {|e| e.save(:validate => false)} if base.persisted? && !options[:binding]
+        end
+      end
+      
+      class ManyToMany < Referenced::Many
+        def <<(*args)
+          options = default_options(args)
+          super(args)
+          base.save(:validate => false) if base.persisted? && !options[:binding]
+        end
+      end
+    end
+  end
+end
