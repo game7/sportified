0eeb54cb69f5019f075784012431e7e0e39b9c90
game7 <cmwoodall@game7.net>
2011-03-07 10:20:53 -0700

    update current_ability to pass Site.current class rather than Site.current.id - provides better handling for nil site

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,7 +2,7 @@ class Admin::AdminController < ApplicationController
   load_and_authorize_resource
 
   def current_ability
-    @current_ability ||= Ability.new(current_user, Site.current.id)
+    @current_ability ||= Ability.new(current_user, Site.current)
   end  
 
 end
