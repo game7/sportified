fb4aa750baa753804a3b9fda39059b667f15cf9e
game7 <cmwoodall@game7.net>
2011-03-30 21:13:31 -0700

    adding carrierwave S3 settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,28 +1,24 @@
 #require 'carrierwave/orm/mongoid'
-#
-#CarrierWave.configure do |config|
-#  config.storage              = :s3  
-#  config.s3_access_key_id     = s3_config['access_key_id']  
-#  config.s3_secret_access_key = s3_config['secret_access_key']  
-#  config.s3_bucket            = s3_config['bucket']  
-#end
-#
+
 if Rails.env.test?  
- CarrierWave.configure do |config|  
-   config.storage = :file  
-   config.enable_processing = false  
- end  
+  CarrierWave.configure do |config|  
+    config.storage = :file  
+    config.enable_processing = false  
+  end  
 end  
 
 if Rails.env.development?  
- CarrierWave.configure do |config|  
-   config.storage = :file  
-   config.enable_processing = true  
- end  
+  CarrierWave.configure do |config|  
+    config.storage = :file  
+    config.enable_processing = true  
+  end  
 end 
 
 if Rails.env.production?  
- CarrierWave.configure do |config|  
-   config.storage = :s3  
- end  
+  CarrierWave.configure do |config|  
+    config.storage = :s3 
+    config.s3_access_key_id     = s3_config['S3_KEY']  
+    config.s3_secret_access_key = s3_config['S3_SECRET']  
+    config.s3_bucket            = s3_config['S3_BUCKET']   
+  end  
 end 
