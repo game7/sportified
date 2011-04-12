ce795c9735f2e1afdece08020db83d596d0c3443
game7 <cmwoodall@game7.net>
2011-04-12 11:12:12 -0700

    add goalie stats to box score

--- vendor/engines/league/app/models/hockey_goaltender.rb
+++ vendor/engines/league/app/models/hockey_goaltender.rb
@@ -10,16 +10,33 @@ class HockeyGoaltender
   field :min_2, :type => Integer, :default => 0
   field :min_3, :type => Integer, :default => 0
   field :min_ot, :type => Integer, :default => 0
+  def min_total
+    min_1 + min_2 + min_3 + min_ot
+  end
 
   field :shots_1, :type => Integer, :default => 0
   field :shots_2, :type => Integer, :default => 0
   field :shots_3, :type => Integer, :default => 0
   field :shots_ot, :type => Integer, :default => 0
+  def shots_total
+    shots_1 + shots_2 + shots_3 + shots_ot
+  end
 
   field :goals_1, :type => Integer, :default => 0
   field :goals_2, :type => Integer, :default => 0
   field :goals_3, :type => Integer, :default => 0
   field :goals_ot, :type => Integer, :default => 0
+  def goals_total
+    goals_1 + goals_2 + goals_3 + goals_ot
+  end
+
+  def saves_total
+    shots_total - goals_total
+  end
+
+  def save_percentage
+    shots_total / saves_total
+  end
 
   embedded_in :hockey_scoresheet
 
@@ -28,17 +45,7 @@ class HockeyGoaltender
 
   scope :left, :where => { :side => 'L' }
   scope :right, :where => { :side => 'R' }
+  scope :for_side, lambda { |side| { :where => { :side => side } } }
 
-  def min_total
-    min_1 + min_2 + min_3 + min_ot
-  end
-
-  def shots_total
-    shots_1 + shots_2 + shots_3 + shots_ot
-  end
-
-  def goals_total
-    goals_1 + goals_2 + goals_3 + goals_ot
-  end
 
 end
