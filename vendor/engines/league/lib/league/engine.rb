f2f395e2b43d0980780179d493047b82ed44e112
game7 <cmwoodall@game7.net>
2011-04-13 15:33:27 -0700

    add autoload paths for league engine

--- vendor/engines/league/lib/league/engine.rb
+++ vendor/engines/league/lib/league/engine.rb
@@ -3,6 +3,9 @@ require 'rails'
 
 module League
   class Engine < Rails::Engine
+    
+    config.autoload_paths += %W( #{config.root}/lib/ )
+    config.autoload_paths += Dir["#{config.root}/lib/**/*"]
 
   end
 end
