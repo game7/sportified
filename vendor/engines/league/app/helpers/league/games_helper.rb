9e194cfdf964157eab5087557666d7cf88e1c8dd
game7 <cmwoodall@game7.net>
2011-04-13 23:37:27 -0700

    fix :side issues in box_score

--- vendor/engines/league/app/helpers/league/games_helper.rb
+++ vendor/engines/league/app/helpers/league/games_helper.rb
@@ -2,7 +2,7 @@ module League::GamesHelper
   
   def show_player_name(statsheet, side, num)
     players = statsheet.players.with_num(num)
-    plr =  side == 'L' ? players.left.first : players.right.first
+    plr =  side == 'left' ? players.left.first : players.right.first
     plr ? plr.name : ''
   end
 
