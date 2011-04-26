3013fd1070406a03da0ea925f58714ee60da3909
game7 <cmwoodall@game7.net>
2011-04-26 14:47:02 -0700

    breadcrumb should not render a link for pages that redirect to child

--- vendor/engines/pages/app/controllers/pages_controller.rb
+++ vendor/engines/pages/app/controllers/pages_controller.rb
@@ -17,7 +17,7 @@ class PagesController < ApplicationController
 
   def set_page_breadcrumbs
     @page.ancestors_and_self.each do |p|
-      add_breadcrumb(p.title, page_friendly_path(p.path))
+      add_breadcrumb(p.title, p.skip_to_first_child ? nil : p.url)
     end
   end
   
