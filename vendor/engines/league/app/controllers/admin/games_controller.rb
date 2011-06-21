0cac118201e2b8335a2a22955db1befd7e2aa388
game7 <cmwoodall@game7.net>
2011-06-21 14:48:58 -0700

    fix game selection criteria

--- vendor/engines/league/app/controllers/admin/games_controller.rb
+++ vendor/engines/league/app/controllers/admin/games_controller.rb
@@ -1,45 +1,45 @@
 class Admin::GamesController < Admin::BaseLeagueController
   
   before_filter :mark_return_point, :only => [:new, :edit, :destroy]
+  
   before_filter :add_games_breadcrumb
+  def add_games_breadcrumb
+    add_breadcrumb 'Games', admin_games_path  
+  end
+  
   before_filter :load_season_options, :only => [:index, :new, :edit]
+  def load_season_options
+    @seasons = Season.for_site(Site.current).desc(:starts_on).entries
+  end
+  
   before_filter :load_division_options, :only => [:index]
-  before_filter :load_team_options, :only => [:new, :edit]
-  before_filter :load_game, :only => [:show, :edit, :destroy, :transition]
-  before_filter :load_games, :only => [:index]
-  before_filter :load_division, :only => [:index]
-  before_filter :load_season, :only => [:index]
-
-  def load_division
-    @division = Division.for_site(Site.current).find(params[:division_id]) if params[:division_id]
-  end
-
-  def load_season
-    @season = Season.for_site(Site.current).find(params[:season_id]) if params[:season_id]    
-  end
-
   def load_division_options
     @divisions = Division.for_site(Site.current).asc(:name).entries
   end
 
-  def load_season_options
-    @seasons = Season.for_site(Site.current).desc(:starts_on).entries
-  end
-
+  before_filter :load_team_options, :only => [:new, :edit]
   def load_team_options
     @teams = Team.for_site(Site.current).asc(:name).entries.collect do |team|
       [ "#{team.name} (#{team.division_name} Division)", team.id ]
     end
   end
 
-  def add_games_breadcrumb
-    add_breadcrumb 'Games', admin_games_path  
-  end
-
+  before_filter :load_game, :only => [:show, :edit, :destroy, :transition]
   def load_game
     @game = Game.for_site(Site.current).find(params[:id])  
   end
+  
+  before_filter :load_division, :only => [:index]
+  def load_division
+    @division = Division.for_site(Site.current).find(params[:division_id]) if params[:division_id]
+  end
+  
+  before_filter :load_season, :only => [:index]
+  def load_season
+    @season = Season.for_site(Site.current).find(params[:season_id]) if params[:season_id]    
+  end
 
+  before_filter :load_games, :only => [:index]
   def load_games
     @date = params[:date] ? Date.parse(params[:date]) : Date.current
     @days_in_future = 14
@@ -52,9 +52,10 @@ class Admin::GamesController < Admin::BaseLeagueController
     @games = Game.for_site(Site.current)    
     @games = @games.for_division(@division) if @division
     @games = @games.for_season(@season) if @season
-    @games = @games.between(@start_date, @end_date) unless @division && @season
+    @games = @games.between(@start_date, @end_date) unless @division.present? || @season.present?
     @games = @games.asc(:starts_on)    
   end
+
 
   # GET /games
   def index
