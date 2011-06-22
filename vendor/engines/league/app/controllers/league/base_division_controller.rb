199ead6188ff17e0344cb2fb3fab94884f6a4168
game7 <cmwoodall@game7.net>
2011-06-21 21:00:22 -0700

    setup nav menu for league (no division) level

--- vendor/engines/league/app/controllers/league/base_division_controller.rb
+++ vendor/engines/league/app/controllers/league/base_division_controller.rb
@@ -11,7 +11,7 @@ class League::BaseDivisionController < League::BaseLeagueController
   end
 
   def set_area_navigation
-    super
+    add_area_ancestor("League", league_path)
     if @division   
       add_area_ancestor("#{@division.name} Division", league_division_path(@division.slug) )  if @division       
       add_area_descendant('Home', league_division_path(@division.slug))
@@ -21,6 +21,13 @@ class League::BaseDivisionController < League::BaseLeagueController
       # TODO: add_area_menu_item('Statistics')
       add_area_descendant('Teams', league_teams_path(@division.slug))
       # TODO: add_area_menu_item('Players')
+    else
+      add_area_descendant('Home', league_path)
+      add_area_descendant('Schedule', league_schedule_path)
+      add_area_descendant('Scoreboard', league_scoreboard_path)
+      #add_area_descendant('Standings', league_standings_path)
+      # TODO: add_area_menu_item('Statistics')
+      add_area_descendant('Teams', league_teams_path)      
     end
 
   end
--- vendor/engines/league/app/controllers/league/base_league_controller.rb
+++ vendor/engines/league/app/controllers/league/base_league_controller.rb
@@ -8,6 +8,12 @@ class League::BaseLeagueController < ApplicationController
   def set_area_navigation
     super
     add_area_ancestor("League", league_path)
+    add_area_descendant('Home', league_path)
+    add_area_descendant('Schedule', league_schedule_path)
+    add_area_descendant('Scoreboard', league_scoreboard_path)
+    #add_area_descendant('Standings', league_standings_path)
+    # TODO: add_area_menu_item('Statistics')
+    add_area_descendant('Teams', league_teams_path)      
   end
 
 end
