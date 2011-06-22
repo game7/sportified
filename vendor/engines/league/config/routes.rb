e3e391394819f2d7094a26fe81db0d8f547b4a6e
game7 <cmwoodall@game7.net>
2011-06-21 20:40:29 -0700

    correct route precedence for league

--- vendor/engines/league/config/routes.rb
+++ vendor/engines/league/config/routes.rb
@@ -1,11 +1,12 @@
 ::Sportified::Application.routes.draw do
 
   match "league" => "league/home#index", :as => :league, :via => :get
-  match "league/:division_slug" => "league/divisions#show", :as => :league_division, :via => :get
+
   match 'league(/:division_slug)/schedule' => 'league/schedule#index', :as => :league_schedule, :via => :get
   match 'league(/:division_slug)/scoreboard' => 'league/scoreboard#index', :as => :league_scoreboard, :via => :get
   match 'league/:division_slug(/:season_slug)/standings' => 'league/standings#index', :as => :league_standings, :via => :get
   match 'league(/:division_slug)(/:season_slug)/teams/' => 'league/teams#index', :as => :league_teams, :via => :get
+  match "league/:division_slug" => "league/divisions#show", :as => :league_division, :via => :get
 
   match 'league/:division_slug/:season_slug/teams/:team_slug' => 'league/teams#show', :as => :league_team, :via => :get
   match 'league/:division_slug/:season_slug/teams/:team_slug/schedule' => 'league/teams#schedule', :as => :league_team_schedule, :via => :get
