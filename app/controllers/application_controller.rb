425ee083e8b5ded08235f7d4f56757661f726194
game7 <cmwoodall@game7.net>
2011-06-28 09:27:13 -0700

    remove redundant CanCan handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
-  end
+
   protect_from_forgery
   helper :layout
 
