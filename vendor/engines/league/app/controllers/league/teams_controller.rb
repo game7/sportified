5a64d4562740dd9e45f96ca7b6e3735ee4e68192
game7 <cmwoodall@game7.net>
2011-07-12 09:53:54 -0700

    related_teams

--- vendor/engines/league/app/controllers/league/teams_controller.rb
+++ vendor/engines/league/app/controllers/league/teams_controller.rb
@@ -68,6 +68,7 @@ class League::TeamsController < League::BaseDivisionSeasonController
     @team_records = @division.team_records.for_season(@season).desc(:pts)
     @current_game = Game.for_team(@team).in_the_future().desc(:starts_on).first
     @next_game = Game.for_team(@team).in_the_future().desc(:starts_on).skip(1).first
+    @related_teams = Team.for_club(@team.club_id).asc(:season_name) if @team.club_id
     @players = @team.players.asc(:last_name)
   end
 
