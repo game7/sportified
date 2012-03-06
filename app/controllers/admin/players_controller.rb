1435a2b0f4544323c1deb9a89edaf74ab5670b52
Chris Woodall <cmwoodall@game7.net>
2012-03-06 00:34:31 -0700

    fix team/players

--- app/controllers/admin/players_controller.rb
+++ app/controllers/admin/players_controller.rb
@@ -4,89 +4,65 @@ class Admin::PlayersController < Admin::BaseLeagueController
   before_filter :load_team, :only => [:index, :new]
   before_filter :add_team_breadcrumbs, :only => [:index]
 
-  def load_team
-    @team = Team.for_site(Site.current).find(params[:team_id])    
-  end
-
-  def add_team_breadcrumbs
-    add_breadcrumb "Teams", admin_teams_path
-    add_breadcrumb "#{@team.name} (#{@team.division_name}-#{@team.season_name})"
-    add_breadcrumb "Players", admin_team_players_path(@team)
-  end
-
-  # GET /players
-  # GET /players.xml
   def index
-
     @players = @team.players.asc(:last_name)
-
-    respond_to do |format|
-      format.html # index.html.erb
-    end
   end
 
-  # GET /players/1
-  # GET /players/1.xml
   def show
     @player = Player.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-    end
   end
 
-  # GET /players/new
-  # GET /players/new.xml
   def new
     @team = Team.find(params[:team_id])
     @player = @team.players.build
-
-    respond_to do |format|
-      format.html # new.html.erb
-    end
   end
 
-  # GET /players/1/edit
   def edit
     @player = Player.find(params[:id])    
   end
 
-  # POST /players
-  # POST /players.xml
   def create
     @team = Team.find(params[:team_id])
     @player = @team.players.build(params[:player])
 
-    respond_to do |format|
-      if @player.save
-        format.html { return_to_last_point(:notice => 'Player was successfully created.') }
-      else
-        format.html { render :action => "new" }
-      end
+    if @player.save
+      return_to_last_point :success => 'Player was successfully created.'
+    else
+      render :action => "new"
     end
+
   end
 
   def update
     @player = Player.find(params[:id])
     @team = @player.team
 
-    respond_to do |format|
-      if @player.update_attributes(params[:player])
-        format.html { return_to_last_point(:notice => 'Player was successfully updated.') }
-      else
-        format.html { render :action => "edit" }
-      end
+    if @player.update_attributes(params[:player])
+     return_to_last_point :success => 'Player was successfully updated.'
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /players/1
   def destroy
     @player = Player.find(params[:id])
     @team = @player.team
     @player.destroy
 
-    respond_to do |format|
-      format.html { return_to_last_point(:notice => 'Player has been deleted.' ) }
-    end
+    return_to_last_point :success => 'Player has been deleted.'
+    
   end
+  
+  private
+  
+  def load_team
+    @team = Team.find(params[:team_id])    
+  end
+
+  def add_team_breadcrumbs
+    add_breadcrumb @team.league_name
+    add_breadcrumb "Teams", admin_teams_path
+    add_breadcrumb @team.name
+    add_breadcrumb "Roster", admin_team_players_path(@team)
+  end  
 end
