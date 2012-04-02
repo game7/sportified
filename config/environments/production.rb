09c0f7f76b2703c471da5d863f627ac86c715ffc
Chris Woodall <cmwoodall@game7.net>
2012-04-02 11:18:30 -0700

    fix wymeditor css precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,9 @@ Sportified::Application.configure do
   
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += [
-    'oia.css'
+    'oia.css',
+    "iframe/default/wymiframe.css",
+    "skins/default/skin.css"
   ]
     
 end
