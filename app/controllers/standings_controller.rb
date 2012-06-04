ccd681ab68e489b5c63ed4c34f2d92d7cd7d9bba
Chris Woodall <cmwoodall@game7.net>
2012-06-04 15:37:02 -0700

    working on pools

--- app/controllers/standings_controller.rb
+++ app/controllers/standings_controller.rb
@@ -12,7 +12,7 @@ class StandingsController < BaseLeagueController
     #  flash[:error] = "Standings layout has not been setup for this division"
     #end
     add_breadcrumb "Standings"
-    @teams = @league.teams.for_season(@season).asc('seed').desc('record.pts').desc('record.w').desc('record.margin')
+    @teams = @league.teams.for_season(@season).asc('division_name').asc('pool').asc('seed').desc('record.pts').desc('record.w').desc('record.margin')
   end
   
   private
