45f3e99ee59705ba317db73c074d0dea17d72b29
Chris Woodall <cmwoodall@game7.net>
2012-12-14 10:47:11 -0700

    block edit functions to dropdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,5 +13,13 @@ module ApplicationHelper
   def icon(name, white = false)
     "<i class=\"icon-#{name.to_s}#{' icon-white' if white}\"></i>".html_safe
   end
+  
+  def vertical_form_for(record, options={}, &block)
+    options[:html] ||= {}
+    options[:html][:class] = "form-vertical #{options[:html][:class]}".rstrip
+
+    form = simple_form_for(record, options, &block) 
+    form.gsub("form-horizontal ", "").html_safe
+  end
 
 end
