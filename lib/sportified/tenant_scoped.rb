16c06f9f9819f11c5c30f7c34f6aaa61556182a0
Chris Woodall <cmwoodall@game7.net>
2013-01-07 16:30:48 -0700

    fix tenant_scoped to mongoid 3

--- lib/sportified/tenant_scoped.rb
+++ lib/sportified/tenant_scoped.rb
@@ -5,20 +5,8 @@ module Sportified
     included do
       belongs_to :tenant, class_name: "::Tenant"
       before_validation { self.tenant ||= Tenant.current }
+      default_scope ->{ where(tenant_id: Tenant.current.id) if ::Tenant.current }
     end
-    
-    module ClassMethods
-      def tenant_scope
-        { :where => { :tenant_id => Tenant.current.id } } if ::Tenant.current
-      end
-      def criteria(embedded = false, scoped = true)
-        (scope_stack.last || Mongoid::Criteria.new(self, embedded)).tap do |crit|
-          return crit.apply_default_scope.fuse(tenant_scope) if scoped
-        end
-      end
-      def is_tenant_scoped?
-        true
-      end
-    end
+
   end
 end
