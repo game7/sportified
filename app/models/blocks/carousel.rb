8d1ad959d6ca2b7252f74f6e1e9ad850d71ce41a
Chris Woodall <cmwoodall@game7.net>
2013-01-10 14:12:45 -0700

    delay image loading for carousel

--- app/models/blocks/carousel.rb
+++ app/models/blocks/carousel.rb
@@ -10,7 +10,7 @@ module Blocks
     field :shuffle, :type => Boolean, :default => false
     
     def posts
-      Post.tagged_with_any(self.tags)
+      Post.tagged_with_any(self.tags).where(:image.exists => true).newest_first.limit(self.post_count)
     end
     
   end
