a04547e17291a7a7c95c8c9e39a5bd512bd6e65e
Chris Woodall <cmwoodall@game7.net>
2013-05-30 23:07:32 -0700

    shut down login for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User
   include Mongoid::Timestamps
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable, 
-          :recoverable, :rememberable, :trackable, :validatable,
-          :omniauthable
+  #devise :database_authenticatable, :registerable, 
+  #        :recoverable, :rememberable, :trackable, :validatable,
+  #        :omniauthable
           
   ## Database authenticatable
   field :email,              :type => String
