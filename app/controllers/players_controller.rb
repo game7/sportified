c22309dfa60c040b8f57292141a54d25ee972329
Chris Woodall <cmwoodall@game7.net>
2015-05-12 12:58:02 -0700

    refactor players page

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,7 +5,7 @@ class PlayersController < BaseLeagueController
 
     @teams = @league.teams.for_season(@season)
     ids = @teams.collect{|team| team.id}
-    @players = Player.where(:team_id.in => ids).asc(:last_name)
+    @players = Player.joins(:team).where('teams.season_id = ?', @season.id).order(last_name: :asc)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -27,7 +27,7 @@ class PlayersController < BaseLeagueController
   private
   
   def get_season_options
-    @season_options = @league.seasons.all.desc(:starts_on).collect{|s| [s.name, players_path(:league_slug => @league.slug, :season_slug => s.slug)]}
+    @season_options = @league.seasons.all.order(starts_on: :desc).collect{|s| [s.name, players_path(:league_slug => @league.slug, :season_slug => s.slug)]}
   end
 
 end
