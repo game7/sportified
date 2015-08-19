b0351b7a394e76571a9e2e3ce436143eec926c5d
Chris Woodall <cmwoodall@game7.net>
2015-08-18 21:50:00 -0700

    add migration tags support for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,7 @@ class Post < ActiveRecord::Base
   scope :newest_first, ->{ desc(:created_at) }
   
   def apply_mongo!(mongo)
+    self.tag_list = mongo[:tags_array].join(',')
     if mongo['image'] && !self.image.url
       puts self.remote_image_url = "https://sportified.s3.amazonaws.com/uploads/#{self.tenant.slug}/posts/#{self.mongo_id}/images/" + mongo['image']
     end
