090495bb5d50b1432b727dbc239d557302dbcfae
Chris Woodall <cmwoodall@game7.net>
2015-08-18 21:04:24 -0700

    correct irreversible db migrations

--- db/migrate/20150713221413_change_hockey_skaters_to_add_defaults.rb
+++ db/migrate/20150713221413_change_hockey_skaters_to_add_defaults.rb
@@ -1,6 +1,5 @@
 class ChangeHockeySkatersToAddDefaults < ActiveRecord::Migration
-  def change
-    
+  def up
     change_column_default :hockey_skaters, :games_played, 0
     change_column_default :hockey_skaters, :goals, 0
     change_column_default :hockey_skaters, :assists, 0
@@ -15,6 +14,21 @@ class ChangeHockeySkatersToAddDefaults < ActiveRecord::Migration
     change_column_default :hockey_skaters, :playmakers, 0
     change_column_default :hockey_skaters, :gordie_howes, 0
     change_column_default :hockey_skaters, :ejections, 0
-        
+  end
+  def down
+    change_column_default :hockey_skaters, :games_played, nil
+    change_column_default :hockey_skaters, :goals, nil
+    change_column_default :hockey_skaters, :assists, nil
+    change_column_default :hockey_skaters, :points, nil
+    change_column_default :hockey_skaters, :penalties, nil
+    change_column_default :hockey_skaters, :penalty_minutes, nil
+    change_column_default :hockey_skaters, :minor_penalties, nil
+    change_column_default :hockey_skaters, :major_penalties, nil
+    change_column_default :hockey_skaters, :misconduct_penalties, nil
+    change_column_default :hockey_skaters, :game_misconduct_penalties, nil
+    change_column_default :hockey_skaters, :hat_tricks, nil
+    change_column_default :hockey_skaters, :playmakers, nil
+    change_column_default :hockey_skaters, :gordie_howes, nil
+    change_column_default :hockey_skaters, :ejections, nil
   end
 end
--- db/migrate/20150713221636_change_hockey_goaltenders_to_add_defaults.rb
+++ db/migrate/20150713221636_change_hockey_goaltenders_to_add_defaults.rb
@@ -1,6 +1,5 @@
 class ChangeHockeyGoaltendersToAddDefaults < ActiveRecord::Migration
-  def change
-    
+  def up
     change_column_default :hockey_goaltenders, :games_played, 0
     change_column_default :hockey_goaltenders, :minutes_played, 0
     change_column_default :hockey_goaltenders, :shots_against, 0
@@ -19,7 +18,27 @@ class ChangeHockeyGoaltendersToAddDefaults < ActiveRecord::Migration
     change_column_default :hockey_goaltenders, :shootout_wins, 0
     change_column_default :hockey_goaltenders, :shootout_losses, 0
     change_column_default :hockey_goaltenders, :total_wins, 0
-    change_column_default :hockey_goaltenders, :total_losses, 0
-        
+    change_column_default :hockey_goaltenders, :total_losses, 0    
+  end
+  def down
+    change_column_default :hockey_goaltenders, :games_played, nil
+    change_column_default :hockey_goaltenders, :minutes_played, nil
+    change_column_default :hockey_goaltenders, :shots_against, nil
+    change_column_default :hockey_goaltenders, :goals_against, nil
+    change_column_default :hockey_goaltenders, :saves, nil
+    change_column_default :hockey_goaltenders, :save_percentage, nil
+    change_column_default :hockey_goaltenders, :goals_against_average, nil
+    change_column_default :hockey_goaltenders, :shutouts, nil
+    change_column_default :hockey_goaltenders, :shootout_attempts, nil
+    change_column_default :hockey_goaltenders, :shootout_goals, nil
+    change_column_default :hockey_goaltenders, :shootout_save_percentage, nil
+    change_column_default :hockey_goaltenders, :regulation_wins, nil
+    change_column_default :hockey_goaltenders, :regulation_losses, nil
+    change_column_default :hockey_goaltenders, :overtime_wins, nil
+    change_column_default :hockey_goaltenders, :overtime_losses, nil
+    change_column_default :hockey_goaltenders, :shootout_wins, nil
+    change_column_default :hockey_goaltenders, :shootout_losses, nil
+    change_column_default :hockey_goaltenders, :total_wins, nil
+    change_column_default :hockey_goaltenders, :total_losses, nil    
   end
 end
