4ad926c6726abbfc4d6fcd78232633bdcb9450c6
Chris Woodall <cmwoodall@game7.net>
2016-02-02 21:10:38 -0700

    fix broken migration

--- db/migrate/20151230173429_add_jersey_number_to_hockey_penalty.rb
+++ db/migrate/20151230173429_add_jersey_number_to_hockey_penalty.rb
@@ -1,5 +1,7 @@
 class AddJerseyNumberToHockeyPenalty < ActiveRecord::Migration
-    change_table :hockey_penalties do |t|
-      t.string :committed_by_number
-    end 
+	def change
+	    change_table :hockey_penalties do |t|
+	      t.string :committed_by_number
+	    end 
+	end
 end
