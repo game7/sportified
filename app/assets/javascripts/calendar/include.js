35ac9678085e63ed6841d174cd5c3d6fc9f44604
Chris Woodall <cmwoodall@game7.net>
2016-02-11 21:10:10 -0700

    remove ng calendar artifiacts

--- app/assets/javascripts/calendar/include.js
+++
@@ -1,8 +0,0 @@
-//= require moment
-//= require angular
-//= require angular-ui-router
-//= require angular-ui-bootstrap-bower
-//= require angular-ui-calendar
-//= require fullcalendar
-//= require angular-rails-templates
-//= require_tree .
