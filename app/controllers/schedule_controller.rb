ceb028c5e04b3b4c6ac3f67f1b30a39d0d13e96c
Chris Woodall <cmwoodall@game7.net>
2016-03-24 15:26:47 -0700

    remove team includes from event schedule

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,10 +1,10 @@
 class ScheduleController < BaseLeagueController
   before_filter :get_team_options, :unless => :all_leagues?
-  
+
   def index
-    
+
     add_breadcrumb("Schedule")
-    
+
     if params[:season_slug]
       add_breadcrumb(@season.name)
       @events = @league.events.for_season(@season).asc(:starts_on)
@@ -17,7 +17,7 @@ class ScheduleController < BaseLeagueController
       @next_date = @date + @days_in_future + @days_in_past
       @prev_date = @date - @days_in_future - @days_in_past
       @events = all_leagues? ? Event : @league.events
-      @events = @events.includes(:location, :home_team, :away_team)
+      @events = @events.includes(:location)
       @events = @events.where('starts_on > ? AND ends_on < ?', @start_date, @end_date).order(starts_on: :asc)
     end
 
@@ -26,16 +26,16 @@ class ScheduleController < BaseLeagueController
       format.xml  { render :xml => @games.entries }
     end
   end
-  
+
   private
-  
+
   def all_leagues?
     !@league
   end
-  
+
   def get_team_options
     @team_options = @league.teams.for_season(@season).order(:name).collect{|t| [t.name, team_schedule_path(:league_slug => t.league.slug, :season_slug => t.season.slug, :team_slug => t.slug)]}
     @team_options.insert 0, ["Team Schedules", ""]
-  end  
+  end
 
 end
