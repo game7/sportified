a4da23b95e3c2e3b3873e1410aa9200f66391d21
Chris Woodall <cmwoodall@game7.net>
2016-03-25 13:57:28 -0700

    sort players by last name

--- app/controllers/admin/hockey_penalties_controller.rb
+++ app/controllers/admin/hockey_penalties_controller.rb
@@ -1,9 +1,9 @@
 class Admin::HockeyPenaltiesController < Admin::BaseLeagueController
-  
+
   before_filter :load_statsheet
   before_filter :load_penalty, :only => [:edit, :update, :destroy]
   before_filter :list_players, :only => [:new, :edit]
-  
+
   def new
     @penalty = Hockey::Penalty.new
   end
@@ -21,7 +21,7 @@ class Admin::HockeyPenaltiesController < Admin::BaseLeagueController
       list_players
     end
   end
-  
+
   def update
     @penalty.update_attributes(hockey_penalty_params)
     if @penalty.save
@@ -34,15 +34,15 @@ class Admin::HockeyPenaltiesController < Admin::BaseLeagueController
   def destroy
     flash[:notice] = "Penalty has been deleted" if @penalty.delete
   end
-  
+
   private
-  
+
   def hockey_penalty_params
-    params.required(:hockey_penalty).permit(:period, :minute, :second, :team_id, :committed_by_id, 
+    params.required(:hockey_penalty).permit(:period, :minute, :second, :team_id, :committed_by_id,
       :infraction, :duration, :severity,
       :start_period, :start_minute, :start_second,
       :end_period, :end_minute, :end_second
-    ) 
+    )
   end
 
   def load_statsheet
@@ -52,9 +52,9 @@ class Admin::HockeyPenaltiesController < Admin::BaseLeagueController
   def load_penalty
     @penalty = @statsheet.penalties.find(params['id'])
   end
-  
+
   def list_players
-    @players = @statsheet.skaters.playing
+    @players = @statsheet.skaters.playing.order(last_name: :asc)
   end
 
 end
