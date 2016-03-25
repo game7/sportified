94e1aee16a115c59dc53024df70c98f58b4e7dec
Chris Woodall <cmwoodall@game7.net>
2016-03-25 12:11:41 -0700

    sort players alphabetically when adding goal

--- app/controllers/admin/hockey_goals_controller.rb
+++ app/controllers/admin/hockey_goals_controller.rb
@@ -1,8 +1,8 @@
 class Admin::HockeyGoalsController < Admin::BaseLeagueController
-  
+
   before_filter :load_statsheet
   before_filter :list_players, :only => [:new, :edit]
-  
+
   def new
     @goal = Hockey::Goal.new{|goal| goal.strength = '5-5' }
   end
@@ -18,31 +18,31 @@ class Admin::HockeyGoalsController < Admin::BaseLeagueController
   end
 
   def destroy
-    
+
     goal = @statsheet.goals.find(params['id'])
-    if goal.delete 
+    if goal.delete
       @statsheet.save
       flash[:notice] = "Goal has been deleted"
     end
 
   end
-  
+
   private
-  
+
   def load_statsheet
     @statsheet = Hockey::Statsheet.find(params['hockey_statsheet_id'])
   end
 
   def prepare_sides
-    @sides = [ [@statsheet.game.away_team_id, @statsheet.game.away_team.name], [@statsheet.game.home_team_id, @statsheet.game.home_team.name] ]    
-  end  
-  
+    @sides = [ [@statsheet.game.away_team_id, @statsheet.game.away_team.name], [@statsheet.game.home_team_id, @statsheet.game.home_team.name] ]
+  end
+
   def hockey_goal_params
     params.required(:hockey_goal).permit(:team_id, :period, :minute, :second, :scored_by_id, :assisted_by_id, :also_assisted_by_id, :strength)
   end
-  
+
   def list_players
-    @players = @statsheet.skaters.playing
-  end  
+    @players = @statsheet.skaters.playing.order(last_name: :asc)
+  end
 
 end
