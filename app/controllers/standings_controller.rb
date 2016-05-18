c48a6b290a90789ee76027601f42a08e2245ca42
Chris Woodall <cmwoodall@game7.net>
2016-05-17 21:05:10 -0700

    Standings Enhancements

--- app/controllers/standings_controller.rb
+++ app/controllers/standings_controller.rb
@@ -2,17 +2,7 @@ class StandingsController < BaseLeagueController
   before_filter :get_season_options
 
   def index
-    #layout = @division.standings_layout
-    #if layout
-    #  @columns = @division.standings_layout.columns.asc(:order)
-    #  @teams = @division.teams.for_season(@season).desc('record.pts').entries
-    #else
-    #  @columns = []
-    #  @teams = []
-    #  flash[:error] = "Standings layout has not been setup for this division"
-    #end
-    add_breadcrumb "Standings"
-    @teams = @division.teams.for_season(@season).order('pool, seed, ' + @division.standings_schema['order'])
+    @teams = @division.teams.for_season(@season)
   end
 
   private
