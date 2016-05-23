3826ed14cc33c1106e3c47d2401543ed9524e199
Chris Woodall <cmwoodall@game7.net>
2016-05-23 13:58:59 -0700

    Reverse Proxy for Client API calls

--- public/client/build/tasks/serve.js
+++ public/client/build/tasks/serve.js
@@ -1,20 +1,27 @@
-var gulp = require('gulp');
+var gulp        = require('gulp');
 var browserSync = require('browser-sync');
+var url         = require('url');
+var proxy       = require('proxy-middleware');
 
 // this task utilizes the browsersync plugin
 // to create a dev server instance
 // at http://localhost:9000
 gulp.task('serve', ['build'], function(done) {
+  var proxyOptions = url.parse('http://localhost:3000/api');
+  proxyOptions.route = '/api';
   browserSync({
     online: false,
     open: false,
     port: 9000,
     server: {
       baseDir: ['.'],
-      middleware: function(req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-      }
+      middleware: [
+        function(req, res, next) {
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          next();
+        },
+        proxy(proxyOptions)
+      ]
     }
   }, done);
 });
