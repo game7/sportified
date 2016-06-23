dd3a4a674bcd1937fb1e0ac8f350e4d29f4b645c
Chris Woodall <cmwoodall@game7.net>
2016-06-23 10:32:07 -0700

    Image Upload Fix
    Uploading of images to an image block (within a page) was failing to
    due renaming of the image uploader to file uploader.  When the rename
    was performed we did not also rename the associated “strong parameters”
    security mechanism and thus the system was blocking the uploaded image
    because it was expecting it to be named “image” as opposed to “file”

--- app/controllers/blocks/images_controller.rb
+++ app/controllers/blocks/images_controller.rb
@@ -16,23 +16,23 @@
 #
 
 class Blocks::ImagesController < BlocksController
-  
+
   def dialog
   end
-  
+
   def edit
   end
-  
-  def update  
+
+  def update
     if @block.update_attributes(blocks_image_params)
       flash[:success] = "Image updated"
-    end    
+    end
   end
-  
+
   private
-  
+
   def blocks_image_params
-    params.required(:blocks_image).permit(:link_url, :alignment, :height, :width, :image, :remote_image_url, :image_cache)
+    params.required(:blocks_image).permit(:link_url, :alignment, :height, :width, :file, :remote_file_url, :file_cache)
   end
-  
+
 end
