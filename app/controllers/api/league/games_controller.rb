315e665f9c0318794a7897e482b1ef1f3030addd
Chris Woodall <cmwoodall@game7.net>
2016-09-13 17:12:15 -0700

    clean up controller

--- app/controllers/api/league/games_controller.rb
+++ app/controllers/api/league/games_controller.rb
@@ -1,25 +1,20 @@
 class Api::League::GamesController < Api::BaseController
 
   def batch_create
-    puts params[:game]
+
     Chronic.time_class = Time.zone
     params[:game].each do |game|
       game[:starts_on] = Chronic.parse(game[:starts_on])
     end
+
     begin
       games = ::League::Game.create! games_params[:game]
       render json: games
     rescue => ex
+      puts games
       render json: ex, status: 400
     end
-    # @game = ::League::Game.new(game_params)
-    # if @game.save
-    #   return_to_last_point :success => 'Game was successfully created.'
-    # else
-    #   flash[:error] = 'Game could not be created.'
-    #   load_options
-    #   render :action => "new"
-    # end
+
   end
 
   private
