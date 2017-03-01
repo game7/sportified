52dc0187758aa2021ba32bc5cd4f4489d39e9dcb
Chris Woodall <cmwoodall@game7.net>
2017-02-28 19:36:30 -0700

    remove deprecated mongoid models

--- app/models/import.rb
+++
@@ -1,9 +0,0 @@
-class Import
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  include Sportified::TenantScoped
-  
-  field :contents, :type => Array
-  validates_presence_of :contents
-    
-end
