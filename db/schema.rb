c0cfc442b03b788a319e5661c1e106013e2d3c9f
Chris Woodall <cmwoodall@game7.net>
2017-03-06 10:17:09 -0700

    add unconfirmed_email column to db. fixes user registration issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302201029) do
+ActiveRecord::Schema.define(version: 20170306171133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -668,6 +668,7 @@ ActiveRecord::Schema.define(version: 20170302201029) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "stripe_customer_id"
+    t.string   "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
