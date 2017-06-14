878f8cc27244c123994c64f08ae08081451e0c01
Chris Woodall <cmwoodall@game7.net>
2017-06-14 15:40:25 -0700

    fix carrierwave fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,7 +17,7 @@ end
 
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.storage = :fog
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['S3_KEY'],
