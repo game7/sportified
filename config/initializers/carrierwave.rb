5def15111a822812550813ab9d06eb41a248053e
Chris Woodall <cmwoodall@game7.net>
2017-06-14 22:27:34 -0700

    storage to fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,6 +17,7 @@ end
 
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       :provider               => 'AWS',
