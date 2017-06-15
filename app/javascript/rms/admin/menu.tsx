880a8186d05432ae4e5715d60b91d43a7a17d4ce
Chris Woodall <cmwoodall@game7.net>
2017-06-15 10:16:28 -0700

    invert top-level menu

--- app/javascript/rms/admin/menu.tsx
+++ app/javascript/rms/admin/menu.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Menu, Container } from 'semantic-ui-react';
 
 export default (props: { location: string }) => (
-  <Menu>
+  <Menu inverted>
     <Container>
       <Menu.Item header>Registrar</Menu.Item>
       <Menu.Item as={Link} to='/' active={props.location == "/"}>Home</Menu.Item>
