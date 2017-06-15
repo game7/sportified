a2d490373365063f31c977c63827098d87eae501
Chris Woodall <cmwoodall@game7.net>
2017-06-14 22:36:42 -0700

    move fog config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -19,12 +19,12 @@ end
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
-    config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['S3_KEY'],
       :aws_secret_access_key  => ENV['S3_SECRET']
     }
     config.fog_directory = ENV['S3_BUCKET']
+    config.storage = :fog    
   end
 end
