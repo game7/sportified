b31b560e8bc454d9b27868c8263372847309a264
Chris Woodall <cmwoodall@game7.net>
2018-07-02 14:15:34 -0700

    soft delete locations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206225812) do
+ActiveRecord::Schema.define(version: 20180702210351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -424,6 +424,8 @@ ActiveRecord::Schema.define(version: 20180206225812) do
     t.string   "mongo_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_locations_on_deleted_at", using: :btree
     t.index ["tenant_id"], name: "index_locations_on_tenant_id", using: :btree
   end
 
