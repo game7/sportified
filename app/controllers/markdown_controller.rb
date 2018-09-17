8fc13e97f18cd584aa9f701464be8c262f551650
Chris Woodall <cmwoodall@game7.net>
2018-09-17 12:00:35 -0700

    posts to display markdown rather than textile

--- app/controllers/markdown_controller.rb
+++ app/controllers/markdown_controller.rb
@@ -2,24 +2,11 @@ class MarkdownController < ApplicationController
   respond_to :json
 
   def preview
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new({
-                                        with_toc_data: true,
-                                        hard_wrap: true
-                                       }),
-                                       autolink: true,
-                                       no_intra_emphasis: true,
-                                       fenced_code_blocks: true,
-                                       strikethrough: true,
-                                       underline: true,
-                                       highlight: true,
-                                       quote: true,
-                                       footnotes: true,
-                                       tables: true)
-    toc = Redcarpet::Markdown.new(Redcarpet::Render::HTML_TOC)
-
-    render json: { markdown: markdown_param,
-                   html: markdown.render(markdown_param),
-                   toc: toc.render(markdown_param)}, status: :ok
+    render json: {
+      markdown: markdown_param,
+      html: MarkdownService.html(markdown_param),
+      toc: MarkdownService.table_of_contents(markdown_param)
+    }, status: :ok
   end
 
   private
---
+++ app/services/markdown_service.rb
@@ -0,0 +1,28 @@
+class MarkdownService
+
+  def self.html(markdown)
+    options = {
+      with_toc_data: true,
+      hard_wrap: true
+    }
+    renderer = Redcarpet::Render::HTML.new(options)
+    extensions = {
+      autolink: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      strikethrough: true,
+      underline: true,
+      highlight: true,
+      quote: true,
+      footnotes: true,
+      tables: true
+    }
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    redcarpet.render(markdown)
+  end
+
+  def self.table_of_contents(markdown)
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML_TOC).render(markdown)
+  end
+
+end
