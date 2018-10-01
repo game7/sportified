3c5673f00f23a7c9e9d5775b38dc574ef572679d
Chris Woodall <cmwoodall@game7.net>
2018-10-01 10:58:58 -0700

    chromecast refresh, locker rooms

--- app/controllers/chromecasts_controller.rb
+++ app/controllers/chromecasts_controller.rb
@@ -5,7 +5,7 @@ class ChromecastsController < ApplicationController
     response.headers['X-FRAME-OPTIONS'] = 'ALLOWALL'
     @chromecast = Chromecast.unscoped.find params[:id]
     Tenant.current = Tenant.find(@chromecast.tenant_id)
-    time = Time.parse('9/8/2018 8:00')
+    # time = Time.parse('9/8/2018 8:00')
     time = Time.zone.now
     @events = Event.where(location: @chromecast.location_id)
                     .ends_after(time)
