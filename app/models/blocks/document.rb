5cffb1fd997b764f35e91f5557a1605a5172acf8
Chris Woodall <cmwoodall@game7.net>
2018-10-02 15:28:10 -0700

    improved document block with icon

--- app/models/blocks/document.rb
+++ app/models/blocks/document.rb
@@ -27,5 +27,35 @@ module Blocks
 
     mount_uploader :file, DocumentUploader
 
+    def extension
+      file&.file&.filename&.split('.')&.last
+    end
+
+    FILE_TYPES = {
+      excel:      %w{xls xlsx},
+      powerpoint: %w{ppt pptx},
+      word:       %w{doc docx},
+      pdf:        %w{pdf},
+      video:      %w{mp4},
+      audo:       %w{mp3},
+      image:      %w{gif jpg jpeg svg}
+    }
+
+    def self.file_types
+      FILE_TYPES
+    end
+
+    def icon
+      result = 'file'
+      FILE_TYPES.each{|key, value| result += "-#{key}" if value.include?(extension) }
+      result
+    end
+
+    before_save :set_default_title
+
+    def set_default_title
+      self.title = self.file.filename if self.title.blank?
+    end
+
   end
 end
