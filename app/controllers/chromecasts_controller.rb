dabe3c1906378ccdf6c7f758c1f34bac27a19b9f
Chris Woodall <cmwoodall@game7.net>
2019-06-04 09:37:31 -0700

    chromecast to show end time; increased padding

--- app/controllers/chromecasts_controller.rb
+++ app/controllers/chromecasts_controller.rb
@@ -6,9 +6,11 @@ class ChromecastsController < ApplicationController
     @chromecast = Chromecast.unscoped.find params[:id]
     @chromecast.update_columns(refreshed_at: Time.current)
     Tenant.current = Tenant.find(@chromecast.tenant_id)
+    @time = Time.zone.now
     @events = Event.where(location: @chromecast.location_id)
-                    .ends_after(Time.zone.now)
-                    .before(Time.zone.now.at_end_of_day)
+                    .ends_after(@time)
+                    .before(@time.at_end_of_day)
                     .order(:starts_on)
+    puts @events.count
   end
 end
