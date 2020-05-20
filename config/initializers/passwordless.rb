955cd4e5195dbd22a5609a085c5bc678eeaf7f8b
game7 <cmwoodall@yahoo.com>
2020-05-20 10:00:46 -0700

    extend passwordless link period

--- config/initializers/passwordless.rb
+++ config/initializers/passwordless.rb
@@ -2,3 +2,6 @@
 
 Passwordless.default_from_address = 'mailer@sportified.net'
 Passwordless.redirect_back_after_sign_in = true
+
+Passwordless.expires_at = lambda { 1.year.from_now } # How long until a passwordless session expires.
+Passwordless.timeout_at = lambda { 1.month.from_now } # How long until a magic link expires.
