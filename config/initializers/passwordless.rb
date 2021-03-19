5b6d53769c82e457f2d522d7e8ff14524bb7a2a3
Chris Woodall <cmwoodall@yahoo.com>
2021-03-19 12:24:36 -0700

    fix contacts/edit modal

--- config/initializers/passwordless.rb
+++ config/initializers/passwordless.rb
@@ -1,7 +1,7 @@
 
 
 Passwordless.default_from_address = 'mailer@sportified.net'
-Passwordless.redirect_back_after_sign_in = true
+Passwordless.redirect_back_after_sign_in = false
 
 Passwordless.expires_at = lambda { 1.year.from_now } # How long until a passwordless session expires.
 Passwordless.timeout_at = lambda { 1.month.from_now } # How long until a magic link expires.
