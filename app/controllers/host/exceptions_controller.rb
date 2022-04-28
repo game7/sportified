07be89a10d5f92a2b982dd76b892aeff81663a98
Chris Woodall <cmwoodall@yahoo.com>
2022-04-28 23:07:31 +0000

    host analytics overhaul

--- app/controllers/host/exceptions_controller.rb
+++ app/controllers/host/exceptions_controller.rb
@@ -2,9 +2,29 @@ class Host::ExceptionsController < Host::HostController
   
   def index
     @exceptions = Ahoy::Event.unscoped.where_event('exception')
-    @by_exception = @exceptions.group('properties #>> \'{exception}\'').count
-    @by_controller = @exceptions.group('CONCAT_WS(\'#\', properties #>> \'{params,controller}\', properties #>> \'{params,action}\')').count
-    @by_host = @exceptions.group('properties #>> \'{host}\'').count
+
+    if params[:exception].present?
+      @exceptions = @exceptions.where_props({ exception: params[:exception] })
+    end
+    
+    if params[:route].present?
+      controller, action = params[:route].split('#')
+      @exceptions = @exceptions.where_props({ params: { controller: controller, action: action } }) if params[:route].present?
+    end
+
+    if params[:hostname].present?
+    @exceptions = @exceptions.where_props({ host: params[:hostname] })
+    end
+
+    if params[:date].present?
+      from = DateTime.parse(params[:date])
+      to = from + 1.day - 1.second
+      @exceptions = @exceptions.where(time: [from..to])
+    end
+
+    @by_exception = @exceptions.group('properties #>> \'{exception}\'').order(Arel.sql('COUNT(*) DESC')).count
+    @by_controller = @exceptions.group('CONCAT_WS(\'#\', properties #>> \'{params,controller}\', properties #>> \'{params,action}\')').order(Arel.sql('COUNT(*) DESC')).count
+    @by_host = @exceptions.group('properties #>> \'{host}\'').order(Arel.sql('COUNT(*) DESC')).count
     @by_day = @exceptions.group_by_day(:time).count
   end
 
--- app/controllers/host/visits_controller.rb
+++ app/controllers/host/visits_controller.rb
@@ -1,10 +1,24 @@
 class Host::VisitsController < Host::HostController
   def index
+    @date = Date.parse(params[:date]) if params[:date]
+    @ip = params[:ip] if params[:ip]
+    
     @visits = Ahoy::Visit.unscoped.all
-    @by_device = @visits.group(:device_type).count
-    @by_os = @visits.group(:os).count
-    @by_browser = @visits.group(:browser).count
-    @visits = @visits.order(started_at: :desc).page(params[:page])
+    
+    @visits = @visits.where(ip: @ip) if @ip
+    @visits = @visits.where(os: params[:device]) if params[:device]
+    @visits = @visits.where(os: params[:os]) if params[:os]
+    @visits = @visits.where(browser: params[:browser]) if params[:browser]
+    @visits = @visits.where(tenant_id: params[:tenant_id]) if params[:tenant_id]
+    
+    @by_device = @visits.group(:device_type).order(Arel.sql('COUNT(*) DESC')).count
+    @by_os = @visits.group(:os).order(Arel.sql('COUNT(*) DESC')).count
+    @by_browser = @visits.group(:browser).order(Arel.sql('COUNT(*) DESC')).count
+    @by_all = @visits.group(:device_type, :os, :browser).order(Arel.sql('COUNT(*) DESC')).count
+    
+    @by_day = @visits.group_by_day(:started_at).count
+
+    @visits = @visits.where(started_at: [@date...(@date + 1.days)]).order(started_at: :asc) if @date
   end
 
   def show
