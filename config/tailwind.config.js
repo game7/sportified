64e753ebf738b71c78f69c59352fa7118c336c2e
Chris Woodall <cmwoodall@yahoo.com>
2022-08-15 17:30:22 +0000

    WIP: theming

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,8 +10,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'site-header': '#000000',
         primary: 'var(--color-primary)',
-        secondary: 'var(--color-secondary)'
+        'primary-focus': 'var(--color-primary-focus)',
+        'primary-content': 'var(--color-primary-content)',
+        secondary: 'var(--color-secondary)',
+        accent: 'var(--color-accent)',
+        neutral: 'var(--color-neutral, )',
+        'neutral-focus': 'var(--color-neutral-focus)',
+        'neutral-content': 'var(--color-neutral-content)'
       },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
