b0c2714a124eef2828cafc6f652ff3c452b17dbe
Chris Woodall <cmwoodall@yahoo.com>
2023-02-07 20:31:06 +0000

    fix monkey patch for acts_as_taggable_on/tag

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -1,3 +1,20 @@
-Rails.application.config.after_initialize do
-  require_relative '../../app/models/acts_as_taggable_on/tag'
+# extend models
+Rails.configuration.to_prepare do
+  module ActsAsTaggableOnTagExtensions # rubocop:disable Lint/ConstantDefinitionInBlock
+    extend ActiveSupport::Concern
+
+    included do
+      scope :for_tenant, lambda { |tenant|
+        ids = ActiveRecord::Base.connection.execute(%(
+            SELECT tag_id
+            FROM "taggings"
+            JOIN "events" ON "taggings"."taggable_id" = "events"."id"
+            WHERE "taggings"."taggable_type" = \'Event\'
+            AND "events"."tenant_id" = #{tenant.id} GROUP BY "tag_id").squish).pluck('tag_id')
+        where(id: ids).order(name: :asc)
+      }
+    end
+  end
+
+  ActsAsTaggableOn::Tag.prepend ActsAsTaggableOnTagExtensions
 end
