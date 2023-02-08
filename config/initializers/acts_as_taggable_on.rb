440aa1d9b2912d9d015d7926602ba73f4ee8ffe1
Chris Woodall <cmwoodall@yahoo.com>
2023-02-08 16:06:12 +0000

    fix for_tenant lookup for tags

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -3,8 +3,8 @@ Rails.configuration.to_prepare do
   module ActsAsTaggableOnTagExtensions # rubocop:disable Lint/ConstantDefinitionInBlock
     extend ActiveSupport::Concern
 
-    included do
-      scope :for_tenant, lambda { |tenant|
+    class_methods do
+      def for_tenant(tenant)
         ids = ActiveRecord::Base.connection.execute(%(
             SELECT tag_id
             FROM "taggings"
@@ -12,7 +12,7 @@ Rails.configuration.to_prepare do
             WHERE "taggings"."taggable_type" = \'Event\'
             AND "events"."tenant_id" = #{tenant.id} GROUP BY "tag_id").squish).pluck('tag_id')
         where(id: ids).order(name: :asc)
-      }
+      end
     end
   end
 
