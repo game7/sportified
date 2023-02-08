ee5c782a68ef1769f54dc591c75475ffecbb1614
Chris Woodall <cmwoodall@yahoo.com>
2023-02-08 05:57:08 +0000

    fix issue with uploading to document block

--- app/controllers/blocks/documents_controller.rb
+++ app/controllers/blocks/documents_controller.rb
@@ -15,23 +15,19 @@
 #
 
 class Blocks::DocumentsController < BlocksController
-  
-  def dialog
-  end
-  
-  def edit
-  end
-  
+  def dialog; end
+
+  def edit; end
+
   def update
-    if @block.update_attributes(blocks_document_params)
-      flash[:success] = "Document updated"
-    end
+    return unless @block.update(blocks_document_params)
+
+    flash[:success] = 'Document updated'
   end
-  
+
   private
-  
+
   def blocks_document_params
     params.required(:blocks_document).permit(:title, :description, :file, :file_cache)
   end
-  
 end
