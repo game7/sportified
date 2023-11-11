0e9dc6349f03395bc6908c9dad523d73f7d0d2ba
Chris Woodall <cmwoodall@yahoo.com>
2023-11-11 17:31:51 +0000

    add boarding to penalty list

--- app/models/hockey/penalty.rb
+++ app/models/hockey/penalty.rb
@@ -31,54 +31,62 @@
 class Hockey::Penalty < ActiveRecord::Base
   include Sportified::TenantScoped
 
-  PERIODS = %w{1 2 3 OT}
-  SEVERITIES = %w[minor major misconduct game_misconduct match]
+  PERIODS = %w[1 2 3 OT].freeze
+  SEVERITIES = %w[minor major misconduct game_misconduct match].freeze
   INFRACTIONS = %w[butt_ending checking_from_behind cross-checking delay_of_game elbowing
-                  fighting holding hooking interference kneeing roughing slashing
-                  spearing tripping unsportsmanlike_conduct misconduct game_misconduct
-                  too_many_men high_stick bench_minor head_contact]
+                   boardning fighting holding hooking interference kneeing roughing slashing
+                   spearing tripping unsportsmanlike_conduct misconduct game_misconduct
+                   too_many_men high_stick bench_minor head_contact].freeze
 
   belongs_to :statsheet, class_name: 'Hockey::Statsheet'
   belongs_to :team, class_name: '::League::Team'
   belongs_to :committed_by, class_name: 'Hockey::Skater::Result'
 
   validates :period, presence: true, inclusion: { in: PERIODS }
-  validates :minute, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 20 }
-  validates :second, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }
+  validates :minute, presence: true,
+                     numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 20 }
+  validates :second, presence: true,
+                     numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }
   validates :infraction, presence: true
-  validates :duration, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 20 }
+  validates :duration, presence: true,
+                       numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 20 }
   validates :severity, presence: true
-  validates :start_minute, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 20 }, allow_nil: true
-  validates :start_second, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }, allow_nil: true
-  validates :end_minute, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 20 }, allow_nil: true
-  validates :end_second, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }, allow_nil: true
+  validates :start_minute,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 20 }, allow_nil: true
+  validates :start_second,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }, allow_nil: true
+  validates :end_minute, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 20 },
+                         allow_nil: true
+  validates :end_second, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 59 },
+                         allow_nil: true
 
   class << self
     def severities
-      SEVERITIES.collect{|i| i.humanize}
+      SEVERITIES.collect { |i| i.humanize }
     end
+
     def infractions
-      INFRACTIONS.collect{|i| i.humanize}
+      INFRACTIONS.collect { |i| i.humanize }
     end
   end
 
   def start_time
-    format_time(self.start_min, self.start_sec)
+    format_time(start_min, start_sec)
   end
 
   def end_time
-    format_time(self.end_min, self.end_sec)
+    format_time(end_min, end_sec)
   end
 
-  scope :sorted_by_time, ->{ order(period: :asc, minute: :desc, second: :desc) }
+  scope :sorted_by_time, -> { order(period: :asc, minute: :desc, second: :desc) }
   scope :for_period, ->(period) { where(per: period) }
 
   def apply_mongo!(mongo)
-    self.team = (mongo['side'] == 'home' ? self.statsheet.home_team : self.statsheet.away_team)
-    self.tenant_id = self.team.tenant_id if self.team
+    self.team = (mongo['side'] == 'home' ? statsheet.home_team : statsheet.away_team)
+    self.tenant_id = team.tenant_id if team
     self.committed_by_number = mongo['plr']
-    self.committed_by = self.statsheet.skaters.where(team_id: self.team_id).where(jersey_number: mongo[:plr]).first if self.team
-    self.tenant_id = self.statsheet.tenant_id
+    self.committed_by = statsheet.skaters.where(team_id: team_id).where(jersey_number: mongo[:plr]).first if team
+    self.tenant_id = statsheet.tenant_id
     self.infraction = mongo['inf']
     self.duration = mongo['dur']
     self.period = mongo['per']
@@ -93,15 +101,15 @@ class Hockey::Penalty < ActiveRecord::Base
   end
 
   def time
-    format_time(self.minute, self.second)
+    format_time(minute, second)
   end
 
   def start_time
-    format_time(self.start_minute, self.start_second)
+    format_time(start_minute, start_second)
   end
 
   def end_time
-    format_time(self.end_minute, self.end_second)
+    format_time(end_minute, end_second)
   end
 
   PER = %w[1 2 3 OT]
@@ -114,8 +122,7 @@ class Hockey::Penalty < ActiveRecord::Base
 
   protected
 
-    def format_time(min, sec)
-      min.to_s + ':' + "0#{sec.to_s}"[-2,2] unless min.blank? || sec.blank?
-    end
-
+  def format_time(min, sec)
+    min.to_s + ':' + "0#{sec}"[-2, 2] unless min.blank? || sec.blank?
+  end
 end
