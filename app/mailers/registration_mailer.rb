96382af4736a0421d8718c31f30bfc3af15ebf40
Chris Woodall <cmwoodall@yahoo.com>
2024-01-23 22:26:34 +0000

    registration: show time and location on event registration email

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,10 +1,9 @@
 class RegistrationMailer < ApplicationMailer
-
   def confirmation_email(id, url)
     @registration = Registration.unscoped.includes(:tenant, variant: :product).find(id)
     @registration_url = url
-    mail(to: @registration.email, subject: "Order Confirmation from #{@registration.tenant.name}")
+    Time.use_zone(@registration.tenant.time_zone) do
+      mail(to: @registration.email, subject: "Order Confirmation from #{@registration.tenant.name}")
+    end
   end
-
 end
-
