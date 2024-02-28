d1f6c4a1beeebb249b374dd1be7edd3f91556867
Chris Woodall <cmwoodall@yahoo.com>
2024-02-28 23:00:02 +0000

    fix n+1 on admin/products page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,16 +1,18 @@
 class Admin::ProductsController < Admin::AdminController
-  skip_before_action :verify_admin, only: [:index, :show]
-  before_action :verify_admin_or_operations, only: [:index, :show]
-  before_action :mark_return_point, :only => [:new, :edit]
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
-  before_action :get_form_packets, only: [:edit, :update, :new, :create]
+  skip_before_action :verify_admin, only: %i[index show]
+  before_action :verify_admin_or_operations, only: %i[index show]
+  before_action :mark_return_point, only: %i[new edit]
+  before_action :set_product, only: %i[show edit update destroy]
+  before_action :get_form_packets, only: %i[edit update new create]
 
   def index
     @products = Product.includes(
-      pending_registrations: [], 
+      registrable: {},
+      pending_registrations: [],
       completed_registrations: [],
       abandoned_registrations: [],
-      cancelled_registrations: []).order(active: :desc, id: :desc)
+      cancelled_registrations: []
+    ).order(active: :desc, id: :desc)
     @products = @products.active unless current_user_is_admin?
   end
 
@@ -22,14 +24,13 @@ class Admin::ProductsController < Admin::AdminController
   def new
     @product = params[:clone] ? Product.find(params[:clone]).dup : Product.new
 
-    @product.assign_attributes( registrable_type: params[:registrable_type], registrable_id: params[:registrable_id] )
-    if(@product.registrable_type == 'Event' && @product.registrable.present?) 
-      @product.title = @product.registrable.summary
-    end
+    @product.assign_attributes(registrable_type: params[:registrable_type], registrable_id: params[:registrable_id])
+    return unless @product.registrable_type == 'Event' && @product.registrable.present?
+
+    @product.title = @product.registrable.summary
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @product = Product.new(product_params)
@@ -37,7 +38,7 @@ class Admin::ProductsController < Admin::AdminController
     if @product.save
       redirect_to [:admin, @product], notice: 'Product was successfully created.'
     else
-      flash[:error] = "Product could not be created"
+      flash[:error] = 'Product could not be created'
       render :new
     end
   end
@@ -46,7 +47,7 @@ class Admin::ProductsController < Admin::AdminController
     if @product.update(product_params)
       redirect_to [:admin, @product], notice: 'Product was successfully updated.'
     else
-      flash[:error] = "Product could not be created"
+      flash[:error] = 'Product could not be created'
       render :edit
     end
   end
@@ -58,24 +59,24 @@ class Admin::ProductsController < Admin::AdminController
 
   protected
 
-    def set_breadcrumbs
-      super
-      add_breadcrumb 'Registration', admin_products_dashboard_path
-      add_breadcrumb 'Products', admin_products_path
-    end  
+  def set_breadcrumbs
+    super
+    add_breadcrumb 'Registration', admin_products_dashboard_path
+    add_breadcrumb 'Products', admin_products_path
+  end
 
   private
 
-    def set_product
-      @product = Product.includes(:variants).find(params[:id])
-    end
+  def set_product
+    @product = Product.includes(:variants).find(params[:id])
+  end
 
-    def product_params
-      params.required(:product).permit(:title, :summary, :description, :quantity_allowed, :active, :private, :roster, :registrable_id, :registrable_type, :image,
-        variants_attributes: [ :id, :title, :description, :form_packet_id, :display_order, :quantity_allowed, :price, :_destroy ])
-    end
+  def product_params
+    params.required(:product).permit(:title, :summary, :description, :quantity_allowed, :active, :private, :roster, :registrable_id, :registrable_type, :image,
+                                     variants_attributes: %i[id title description form_packet_id display_order quantity_allowed price _destroy])
+  end
 
-    def get_form_packets
-      @form_packets = FormPacket.all
-    end
+  def get_form_packets
+    @form_packets = FormPacket.all
+  end
 end
